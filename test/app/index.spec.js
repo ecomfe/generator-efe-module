836d94be75358fe5e424d23e4b5271b8aed611d7
nighca <nighca@live.cn>
2014-12-03 17:56:24 +0800

    add test cases

--- test/app/index.spec.js
+++ test/app/index.spec.js
@@ -11,12 +11,10 @@ describe('ecomfe-module:app', function () {
 
         runs(function () {
             helpers.run(path.join(__dirname, '../../app'))
-                .inDir(path.join(os.tmpdir(), './temp-test'))
-                // .withOptions({'skip-install': true})
+                .inDir(path.join(os.tmpdir(), './temp-test1'))
                 .withPrompt({
                     githubUser: 'nighca',
-                    moduleName: 'test-module',
-                    doInstall: false
+                    moduleName: 'test-module'
                 }).on('end', function () {
                     done = true;
                 });
@@ -39,4 +37,76 @@ describe('ecomfe-module:app', function () {
             ]);
         });
     });
+
+    it('creates files in curr dir if module name matches dirname', function () {
+        var done = false;
+
+        runs(function () {
+            helpers.run(path.join(__dirname, '../../app'))
+                .inDir(path.join(os.tmpdir(), './temp-test2'))
+                .withPrompt({
+                    githubUser: 'nighca',
+                    moduleName: 'temp-test2'
+                }).on('end', function () {
+                    done = true;
+                });
+        });
+
+        waitsFor(function() {
+            return done;
+        });
+
+        runs(function() {
+            assert.file([
+                '.editorconfig',
+                '.eslintrc',
+                '.gitignore',
+                '.npmignore',
+                '.travis.yml',
+                'package.json',
+                'README.md',
+                'LICENSE'
+            ]);
+        });
+    });
+
+    it('use empty info with wrong github account', function () {
+        var done = false;
+
+        runs(function () {
+            helpers.run(path.join(__dirname, '../../app'))
+                .inDir(path.join(os.tmpdir(), './temp-test3'))
+                .withPrompt({
+                    githubUser: 'SOMEBODY_DOES_NOT_EXIST',
+                    moduleName: 'test-module'
+                }).on('end', function () {
+                    done = true;
+                });
+        });
+
+        waitsFor(function() {
+            return done;
+        });
+
+        runs(function() {
+            assert.file([
+                '.editorconfig',
+                '.eslintrc',
+                '.gitignore',
+                '.npmignore',
+                '.travis.yml',
+                'package.json',
+                'README.md',
+                'LICENSE'
+            ]);
+
+            var packageInfo = require(path.join(os.tmpdir(), './temp-test', 'test-module/package.json'));
+            assert.deepEqual(packageInfo.author, {
+                name: '',
+                email: '',
+                url: ''
+            });
+        });
+    });
+
 });
